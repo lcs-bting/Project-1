61f493a1d1672fc2a947d8a52311f86ed7a8bb2d
BIll Ting <bting@lcs.on.ca>
2020-11-04 10:02:46 -0500

    Initial Commit

---
+++ Project 1/Project_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Project_1App.swift
+//  Project 1
+//
+//  Created by Bill Ting on 2020-11-04.
+//
+
+import SwiftUI
+
+@main
+struct Project_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
