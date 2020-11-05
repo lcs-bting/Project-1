9476011fbacd9895b08f3eb4bf1a97f7f276b67b
BIll Ting <bting@lcs.on.ca>
2020-11-04 20:22:45 -0500

    Picker done

--- Project 1/ContentView.swift
+++ Project 1/ContentView.swift
@@ -8,11 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let temperatures = ["Celsius", "Fahrenheit", "Kelvin"]
+    @State private var selectedTemperature = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Picker("Select the user", selection: $selectedTemperature) {
+                ForEach(0 ..< temperatures.count) {
+                    Text(self.temperatures[$0])
+                }
+            }
+            Text("You chose: Temperature # \(temperatures[selectedTemperature])")
+        }
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
