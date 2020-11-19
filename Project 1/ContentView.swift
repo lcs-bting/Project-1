e93627fcc5055dae3c1e9b945d499487edf0fe6d
BIll Ting <bting@lcs.on.ca>
2020-11-19 10:02:13 -0500

    redo day 19

--- Project 1/ContentView.swift
+++ Project 1/ContentView.swift
@@ -8,30 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var selectedTemperature = 1
-    @State private var selectedLength = 2
-    @State private var selectedTime = 1
-    @State private var selectedVolume = 1
+    @State private var fromUnitType = 2
+    @State private var toUnitType =  2
+    @State private var currentValue = ""
     
-    let temperatureStyles = ["Celsius", "Fahrenheit", "Kelvin"]
     let lengthStyles = [ "Meters", "Kilometers", "Feet", "Yards", "Miles"]
-    let timeStyles = [ "Seconds", "Minutes", "Hours", "Days"]
-    let volumeStyles = [ "Milliliters", "Liters", "Cups", "Pints", "Gallons"]
     
     var body: some View {
         NavigationView{
             Form {
-                Section(header: Text("What Temperature Styles Do You Prefer?")) {
-                    Picker("Temperature Style", selection: $selectedTemperature) {
-                        ForEach(0 ..< temperatureStyles.count) {
-                            Text("\(self.temperatureStyles[$0])")
-                        }
-                    }
-                    .pickerStyle(SegmentedPickerStyle())
+                Section(header: Text("What Is The Current Value?")) {
+                    TextField("Amount", text: $currentValue)
+                        .keyboardType(.decimalPad)
                 }
                 
-                Section(header: Text("What Lengh Style Do You Prefer?")) {
-                    Picker("Length Style", selection: $selectedLength) {
+                Section(header: Text("What Unit Is The Current Value In?")) {
+                    Picker("Length Style", selection: $fromUnitType) {
                         ForEach(0 ..< lengthStyles.count) {
                             Text("\(self.lengthStyles[$0])")
                         }
@@ -39,19 +31,10 @@ struct ContentView: View {
                     .pickerStyle(SegmentedPickerStyle())
                 }
                 
-                Section(header: Text("What Time Styles Do You Prefer?")) {
-                    Picker("Time Style", selection: $selectedTime) {
-                        ForEach(0 ..< timeStyles.count) {
-                            Text("\(self.timeStyles[$0])")
-                        }
-                    }
-                    .pickerStyle(SegmentedPickerStyle())
-                }
-                
-                Section(header: Text("What Volume Styles Do You Prefer?")) {
-                    Picker("Volume Style", selection: $selectedVolume) {
-                        ForEach(0 ..< volumeStyles.count) {
-                            Text("\(self.volumeStyles[$0])")
+                Section(header: Text("Convert To What Unit?")) {
+                    Picker("Length Style", selection: $toUnitType) {
+                        ForEach(0 ..< lengthStyles.count) {
+                            Text("\(self.lengthStyles[$0])")
                         }
                     }
                     .pickerStyle(SegmentedPickerStyle())
